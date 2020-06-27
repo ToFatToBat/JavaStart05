e87e9b374c401a3037459208a2f5e111543103d8
ToFatToBat <kpapiernia@wp.pl>
2020-06-27 17:38:59 +0200

    abc

--- src/homework/year/App.java
+++ src/homework/year/App.java
@@ -3,10 +3,10 @@ package homework.year;
 public class App {
     public static void main(String[] args) {
 
-        Year year1 = new Year(2000);
-        Year year2 = new Year(1684);
-        Year year3 = new Year(2100);
-        Year year4 = new Year(1600);
+        int year1 = 2000;
+        int year2 = 1684;
+        int year3 = 2100;
+        int year4 = 1600;
 
 
         YearCheck yearCheck = new YearCheck();
--- src/homework/year/YearCheck.java
+++ src/homework/year/YearCheck.java
@@ -2,10 +2,10 @@ package homework.year;
 
 public class YearCheck {
 
-    public boolean isLeap(Year year) {
-        int rest1 = year.getYear() % 4;
-        int rest2 = year.getYear() % 100;
-        int rest3 = year.getYear() % 400;
+    public boolean isLeap(int year1) {
+        int rest1 = year1 % 4;
+        int rest2 = year1 % 100;
+        int rest3 = year1 % 400;
         boolean result;
         if (rest1 == 0 && rest2 != 0) {
             result = true;
